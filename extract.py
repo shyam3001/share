0dc6d8e22615219d1ed539d61b96c26e4846f632
Shyam Reyal <shyam3001@gmail.com>
2021-05-18 00:22:03 +0530

    added extract.py

---
+++ extract.py
@@ -0,0 +1,16 @@
+import os
+import re
+import sys
+
+if len(sys.argv)==1:
+    print('usage: python extract.py foldername')
+    exit(0)
+
+list = os.listdir(sys.argv[1])
+
+for name in list:
+    itnum = re.findall('[iI][tT][0-9]{8}', name);
+    if len(itnum)==1:
+        print(itnum[0])
+    else:
+        print('could not find IT number in this folder = ', name)
