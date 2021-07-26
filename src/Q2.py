9ff02281a7dd0c2124211a7a2207e9fa97ce22d3
Shyam Reyal <shyam3001@gmail.com>
2021-07-26 13:29:22 +0530

    added python solution

---
+++ src/Q2.py
@@ -0,0 +1,31 @@
+import csv
+
+with open('File1.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    data1 = list(csv_reader)
+    headers1 = [data1[0]]
+    list1 = data1[1:]
+    dict1 = {list1[x][0]: list1[x][1:3] for x in range(len(list1))}
+
+with open('File2.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    data2 = list(csv_reader)
+    headers2 = [data2[0]]
+    list2 = data2[1:]
+    dict2 = {list2[x][0]: [list2[x][1]] for x in range(len(list2))}
+
+with open('File3.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    data3 = list(csv_reader)
+    headers3 = [data3[0]]
+    list3 = data3[1:]
+    dict3 = {list3[x][0]: [list3[x][1]] for x in range(len(list3))}
+
+result = {}
+
+for key in set().union(dict1, dict2, dict3):
+    if key in dict1: result.setdefault(key, []).extend(dict1[key])
+    if key in dict2: result.setdefault(key, []).extend(dict2[key])
+    if key in dict3: result.setdefault(key, []).extend(dict3[key])
+
+print(result)
