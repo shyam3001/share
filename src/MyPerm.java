0f3689ef53093dd5556e8ca16b4734fb26c1c691
Shyam Reyal <shyam3001@gmail.com>
2021-08-07 12:53:33 +0530

    added comments to MyPerm.java

--- src/MyPerm.java
+++ src/MyPerm.java
@@ -1,26 +1,27 @@
 import java.util.Arrays;
 
 public class MyPerm {
-    static int[] inp = {1,2,3,4};
+    static int[] inp = {1,2,3};
     static boolean[] b = new boolean[inp.length];
     static int[] ans = new int[inp.length];
     static int a = 0;
 
     static void rec(int pos) {
-        b[pos] = true;
-        ans[a++] = inp[pos];
+        b[pos] = true;          // make position unavailable as we recurse forward
+        ans[a++] = inp[pos];    // append the current number to the answer
+
+        // if we have the full length, then print the answer
         if (a == inp.length) {
             System.out.println(Arrays.toString(ans));
         }
 
+        // recurse to the next available position
         for (int i=0; i<inp.length; i++) {
-            if (!b[i]) {
-                rec(i);
-            }
+            if (!b[i]) rec(i);
         }
 
-        a--;
-        b[pos] = false;
+        a--;               // redact the answer length, as we backtrack
+        b[pos] = false;    // make position available again
     }
 
     public static void main(String[] args) {
