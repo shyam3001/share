4d07d299d11b5e0d10b2ccfcbb4db56ae47a1220
Shyam Reyal <shyam3001@gmail.com>
2021-08-07 12:54:26 +0530

    removed exception handling

--- src/BasicDFS.java
+++ src/BasicDFS.java
@@ -28,30 +28,28 @@ public class BasicDFS {
     }
 
     public static void main(String[] args) throws Exception {
-        try(Scanner in = new Scanner(new File("graph.in"))) {
-            N = in.nextInt();
-            E = in.nextInt();
-            arr = new int[N+1][N+1];
-            visit = new boolean[N+1];
-            path = new int[N*N];
+        Scanner in = new Scanner(new File("graph.in"));
+        N = in.nextInt();
+        E = in.nextInt();
+        arr = new int[N+1][N+1];
+        visit = new boolean[N+1];
+        path = new int[N*N];
 
-            for (int i=0; i<E; i++) {
-                int st = in.nextInt();
-                int et = in.nextInt();
-                int w = in.nextInt();
-                arr[st][et] = w;
-            }
-            for (int i=1; i<=N; i++) {
-                for (int j = 1; j <= N; j++)
-                    System.out.print(arr[i][j] + " ");
-                System.out.println();
-            }
+        for (int i=0; i<E; i++) {
+            int st = in.nextInt();
+            int et = in.nextInt();
+            int w = in.nextInt();
+            arr[st][et] = w;
+        }
+        in.close();
+
+        for (int i=1; i<=N; i++) {
+            for (int j = 1; j <= N; j++)
+                System.out.print(arr[i][j] + " ");
             System.out.println();
+        }
+        System.out.println();
 
-            rec(1,6);
-        }
-        catch(Exception e) {
-            e.printStackTrace();
-        }
+        rec(1,6);
     }
 }
