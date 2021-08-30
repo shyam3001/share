57b834adbac563f3724d91e40929955d448864b1
Shyam Reyal <shyam3001@gmail.com>
2021-08-30 12:30:00 +0530

    optimize code

--- src/LogicManager.java
+++ src/LogicManager.java
@@ -2,7 +2,7 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class LogicManager {
     private final FileManager fileManager;
-    private ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue(5);
+    private final ArrayBlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(5);
 
     public LogicManager(FileManager fileManager) {
         this.fileManager = fileManager;
@@ -13,7 +13,7 @@ public class LogicManager {
     }
 
     public void consumeText() {
-        String text = (String)blockingQueue.poll();
+        String text = blockingQueue.poll();
         if (text != null) {
             fileManager.writeToFile(text);
         }
