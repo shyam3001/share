48581ea387d0f1b3029ae8aaec766e24f9def569
Shyam Reyal <shyam3001@gmail.com>
2021-10-08 16:13:03 +0530

    count-chats to take user input

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Repository to share code with others
 
 ## count-chats.py
-* Counts the number of chats sent by a student from the zoom downloaded chat log `GMT20211006-034039_Recording.txt` and produces a CSV with a similar name `GMT20211006-034039_Recording_counts.csv`
+* Counts the number of chats sent by a student from the zoom downloaded chat log `GMT20211006-034039_Recording.txt` and produces a CSV with a matched name `GMT20211006-053150_Recording.txt_counts.csv`
 
 ## count-att.py
 * merges the attendance durations from the zoom downloaded attendance log `participants_98226248040.csv` into cumulative counts `attendance_98226248040.csv`
--- count-chats.py
+++ count-chats.py
@@ -1,24 +1,42 @@
 import os, re
 
+print('')
+print('--------------------------------------------------------------------')
+print('')
+print('Welcome to the chat counter\n')
+
 list = [ file for file in os.listdir('.') if file.endswith('_Recording.txt') ]
 
-for file in list:
-    fin = open(file,'r')
-    
-    result = []
-    lines = fin.readlines()
-    for line in lines:
-        names = re.split(r'\t', line) 
-        if len(names) == 3:
-            name = names[1][:-1]
-            result.append(name)
+print('We found {0} files matching the Zoom chat log:'.format(len(list)))
+for file_in in list:
+    print(' - ' + file_in)
+print('')
 
-    fout = open(file[:-4]+'_counts.csv', 'w')
-    fout.write('Name, Count\n')
+file_in = input('Enter input file name: ')
+file_out = file_in+'_counts.csv'
 
-    result = {x: result.count(x) for x in set(result) }
-    for k,v in result.items():
-        fout.write(k + "," + str(v) + '\n')
-    
-    fin.close()
-    fout.close()
+fin = open(file_in,'r')
+
+result = []
+lines = fin.readlines()
+for line in lines:
+    names = re.split(r'\t', line) 
+    if len(names) == 3:
+        name = names[1][:-1]
+        result.append(name)
+
+fout = open(file_out, 'w')
+fout.write('Name, Count\n')
+
+result = {x: result.count(x) for x in set(result) }
+for k,v in result.items():
+    fout.write(k + "," + str(v) + '\n')
+
+fin.close()
+fout.close()
+
+print('')
+print('Written interaction count to output file: {0}'.format(file_out))
+print('')
+print('--------------------------------------------------------------------')
+print('')
