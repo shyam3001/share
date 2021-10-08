b9aebde5924987ef48266eb8d3afd78e65ed32d5
Shyam Reyal <shyam3001@gmail.com>
2021-10-08 12:43:49 +0530

    initial commit

---
+++ count-att.py
@@ -0,0 +1,26 @@
+import os, re
+
+list = [ file for file in os.listdir('.') if file.startswith('participants') ]
+
+for file in list:
+    fin = open(file,'r')
+
+    lines = sorted(fin.readlines()[1:])
+    dict = {}
+    for line in lines:
+        line = line.split(',')
+        if len(line) > 4:
+            k = line[0]
+            v = int(line[4])
+            if k in dict:
+                dict[k] = dict[k] + v
+            else:
+                dict[k] = v
+
+    fout = open('attendance_'+file[13:], 'w')
+    for k, v in dict.items():
+        res = '{0}, {1}h {2}m\n'.format(k, v//60, v%60)
+        fout.write(res)
+
+    fin.close()
+    fout.close()
---
+++ count-chats.py
@@ -0,0 +1,23 @@
+import os, re
+
+list = [ file for file in os.listdir('.') if file.endswith('_Recording.txt') ]
+
+for file in list:
+    fin = open(file,'r')
+    
+    result = []
+    lines = fin.readlines()
+    for line in lines:
+        names = re.split(r'\t', line) 
+        if len(names) == 3:
+            name = names[1][:-1]
+            result.append(name)
+
+    fout = open(file[:-4]+'_counts.csv', 'w')
+
+    result = {x: result.count(x) for x in set(result) }
+    for k,v in result.items():
+        fout.write(k + "," + str(v) + '\n')
+    
+    fin.close()
+    fout.close()
